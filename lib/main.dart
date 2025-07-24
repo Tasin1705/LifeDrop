155a4bc20b1ee753d30dbf4cfbce53a517bee00f
Tasin1705 <ahnaftasinsig@gmail.com>
2025-07-24 23:03:57 +0600

    Tasin

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:first_app/hospital_dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:first_app/homepage/home_page.dart';
 import 'package:first_app/dashboard/donor_dashboard.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -19,6 +21,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const HomePage(),
         '/dashboard': (context) => const DonorDashboard(),
+        '/hospital-dashboard': (context) => const HospitalDashboard(),
       },
     );
   }
